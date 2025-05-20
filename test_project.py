2008d25d54c89fbc35122b7ed4659d1c8936fba3
Svish23 <sevators@gmail.com>
2025-05-20 18:32:26 +0300

    Remove test_project.py

--- test_project.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from game import game_start
-
-root = tk.Tk()
-starting_window = tk.Frame(root,width=800,height=600)
-
-def test_difficulty():
-    assert game_start(root,"Easy",starting_window) == "Easy"
